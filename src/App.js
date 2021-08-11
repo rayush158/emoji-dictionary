18ad007729b9aa71e188c4c44a8a88492dcbd14e
ayushrajput158 <87794509+ayushrajput158@users.noreply.github.com>
2021-08-11 22:06:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "😲": "disbelief",
+  "😥": "sad",
+  "🥡": "takeout box",
+  "🥰": "love",
+  "😑": "annoyance",
+  "🔥": "fire"
+};
+var emojiWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  function emojiInputHandler(event) {
+    //processing
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "We don't have this in our database";
+    }
+    setMeaning(meaning); // react call to show output
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning); // react call to show output
+  }
+  return (
+    <div className="App">
+      <h1>Insite outtt</h1>
+      <input onChange={emojiInputHandler}></input>
+      <h2>{meaning}</h2>
+      {/* Actual value set by react using useState */}
+      <h3> Emojis We Know </h3>
+      {emojiWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5Rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
